893934be3045ed4b249262a7932ecc52f55ca6be
Alex <alexmozagba@gmail.com>
2025-06-19 18:59:08 +0200

    initial commit try 2

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,12 +1,7 @@
-
-
 export default function Contact() {
-     return (
-
-
-          <main>
-               <h1>to get in touch</h1>
-          </main>
-
-     )
+  return (
+    <main>
+      <h1>to get in touch</h1>
+    </main>
+  );
 }
--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -1,13 +1,9 @@
-
-
-
-
 export default function Work() {
-     return (
-          <>
-               <main>
-                    <h1>this is the home</h1>
-               </main>
-          </>
-     )
+  return (
+    <>
+      <main>
+        <h1>this is the home</h1>
+      </main>
+    </>
+  );
 }
