c4178d2b33a2c1a3bea7eae113c743785de65bc6
Alex <alexmozagba@gmail.com>
2025-07-12 14:35:42 +0200

    changed the menuexpanding and colapsing code to functions that might make it easier for me to animate

--- src/comp/MainNav/MainNav.tsx
+++ src/comp/MainNav/MainNav.tsx
@@ -7,78 +7,41 @@ import { useEffect, useRef, useState } from "react";
 gsap.registerPlugin(useGSAP);
 
 export default function MainNav() {
-
-  const [isHovered, setIsHovered] = useState(false)
+  const [navListClass, setNavListClass] = useState(`${styles.ul}`)
+  const [langClass, setLangClass] = useState(`${styles.langCont}`)
   const [isClicked, setIsClicked] = useState(false);
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
 
   const menuBtnRef = useRef<HTMLDivElement>(null);
-  const topBarRef = useRef<SVGRectElement>(null);
-  const midBarRef = useRef<SVGRectElement>(null);
-  const bottomBarRef = useRef<SVGRectElement>(null);
+  const menuVectorRef = useRef<SVGSVGElement>(null);
   const headerRef = useRef<HTMLElement>(null);
   const logoRef = useRef<HTMLAnchorElement>(null);
   const hShapeRef = useRef<HTMLDivElement>(null);
   const navLinkRef = useRef<HTMLLIElement>(null);
-  // let rspSize
-  // let xPos
 
-  let tl = gsap.timeline();
+  let tl
 
-  // useGSAP(() => {
-  //   const heEl = headerRef?.current;
-  //   if (!heEl) return;
-  //   tl.clear();
+  useEffect(() => {
+    menuAnim()
 
-  //   if (width <= 500) {
-  //     isClicked ? tl.to(heEl, { duration: 0.3, height: '12.85rem' }) : tl.to(heEl, { duration: 0.3, height: '1.7rem' });
-
-  //   } else if (width > 500 && width <= 1024) {
-  //     isClicked ? tl.to(heEl, { duration: 0.3, height: '11.5rem' }) : tl.to(heEl, { duration: 0.3, height: '1.7rem' });
-
-  //   } else if (width > 1024 && !isClicked && heEl) {
-  //     tl.to(heEl, { duration: 0.3, height: '2.25rem' });
-
-  //   }
-
-  // }, [isClicked, width])
-
-  // useGSAP(() => {
-  //   // setMenuText(isClicked ? "CLOSE" : "MENU");
-
-  //   tl.clear()
-  //   if (topBarRef?.current && bottomBarRef?.current && midBarRef?.current && isClicked) {
-  //     tl.to(topBarRef.current, { duration: 0.3, rotate: 45, y: 7, ease: "power4.inOut" })
-  //       .to(midBarRef.current, { duration: 0.3, opacity: 0 }, 0)
-  //       .to(bottomBarRef.current, { duration: 0.3, rotate: -45, y: -7, ease: "power4.inOut" }, 0)
-  //   };
-
-
-  // }, { dependencies: [isClicked], scope: menuBtnRef, revertOnUpdate: true });
-
+  }, [isClicked, width])
 
 
   useEffect(() => {
 
 
     window.addEventListener("resize", handleResize);
-    window.addEventListener("click", (e) => {
-      if (menuBtnRef?.current && !menuBtnRef.current.contains(e.target as Node)) {
-        setIsClicked(false);
-        console.log(e.target)
-      }
-    });
-
+    window.addEventListener("pointerdown", handleClickOuside);
+    document.addEventListener("keydown", handleEscape);
+    document.addEventListener("keydown", handleEnter);
 
 
     return () => {
       window.removeEventListener("resize", handleResize);
-      window.removeEventListener("click", (e) => {
-        if (e.target !== menuBtnRef.current) {
-          setIsClicked(false);
-        }
-      });
+      window.addEventListener("pointerdown", handleClickOuside);
+      document.addEventListener("keydown", handleEscape);
+      document.addEventListener("keydown", handleEnter);
     };
   }, []);
 
@@ -113,33 +76,45 @@ export default function MainNav() {
     setWidth(window.innerWidth);
     setHeight(window.innerHeight);
     setIsClicked(false);
+    console.log(isClicked, width)
+
   };
 
-  function menuBtnClick() {
+  function menuBtnClick(e: MouseEvent) {
+    e.preventDefault()
     setIsClicked((c) => !c);
+
   };
 
-  // function expandMenu() {
-  //   if (topBarRef?.current && bottomBarRef?.current && midBarRef?.current) {
-  //     tl.to(topBarRef.current, { duration: 0.3, rotate: 45, y: 7, ease: "power4.inOut" })
-  //       .to(midBarRef.current, { duration: 0.3, opacity: 0 }, 0)
-  //       .to(bottomBarRef.current, { duration: 0.3, rotate: -45, y: -7, ease: "power4.inOut" }, 0)
-  //   };
-  // }
-  // function colapseMenu() {
-  //   if (topBarRef?.current && bottomBarRef?.current && midBarRef?.current) {
-  //     tl.to(topBarRef.current, { duration: 0.3, rotate: -45, y: 7, ease: "power4.inOut" })
-  //       .to(midBarRef.current, { duration: 0.3, opacity: 1 }, 0)
-  //       .to(bottomBarRef.current, { duration: 0.3, rotate: 45, y: -7, ease: "power4.inOut" }, 0)
-  //   };
-  // }
+  function handleClickOuside(e: MouseEvent) {
+    e.preventDefault()
+    if (menuBtnRef?.current && !menuBtnRef.current.contains(e.target as Node)) {
+      setIsClicked(false);
+    }
+  };
+  function handleEnter(e: KeyboardEvent) {
+    if (menuBtnRef?.current && e.key === 'Enter' && menuBtnRef.current.contains(e.target as Node)) {
+      setIsClicked((c) => !c);
+    }
+  };
+
+  function handleEscape(e: KeyboardEvent) {
+    if (headerRef?.current && e.key === 'Escape' && headerRef.current.contains(e.target as Node)) {
+      setIsClicked(false);
+    }
+  };
+
+  function menuAnim() {
+    setNavListClass(isClicked ? `${styles.expandedNav}` : `${styles.ul}`)
+    setLangClass(!isClicked && width <= 1024 ? styles.inactive : styles.langCont)
+  }
 
 
 
-  const showMenu = width <= 1024 ? !isClicked : true;
-  const ulClass = showMenu ? `${styles.ul}` : `${styles.expandedNav}`;
-  const langClass =
-    !isClicked && width <= 1025 ? styles.inactive : styles.langCont;
+  // const showMenu = width <= 1024 ? isClicked : true;
+
+  // const langClass = !isClicked && width <= 1025 ? styles.inactive : styles.langCont;
+
 
   return (
     <header ref={headerRef}>
@@ -159,22 +134,20 @@ export default function MainNav() {
             height="19"
             viewBox="0 0 25 19"
             xmlns="http://www.w3.org/2000/svg"
+            ref={menuVectorRef}
           >
             <g transform="translate(0, 0)">
-              <rect ref={topBarRef} width="100%" height="3" x="0" y="0" fill="#1A56FF" stroke="none" />
-              <rect ref={midBarRef} width="100%" height="3" x="0" y="8" fill="#1A56FF" stroke="none" />
-              <rect ref={bottomBarRef} width="100%" height="3" x="0" y="16" fill="#1A56FF" stroke="none" />
+              <rect className="topBar" width="100%" height="3" x="0" y="0" fill="#1A56FF" stroke="none" />
+              <rect className="midBar" width="100%" height="3" x="0" y="8" fill="#1A56FF" stroke="none" />
+              <rect className="bottomBar" width="100%" height="3" x="0" y="16" fill="#1A56FF" stroke="none" />
             </g>
           </svg>
         </div>
 
-        <ul key="nav-list" aria-label="main navigation" className={ulClass}>
+        <ul key="nav-list" aria-label="main navigation" className={navListClass}>
           {navLinks.map((link) => (
             <li key={link.name}
               ref={navLinkRef}
-
-            // onMouseLeave={handleUnhover}
-            // onBlur={handleUnhover}
             >
               <NavLink
                 className={styles.a}
